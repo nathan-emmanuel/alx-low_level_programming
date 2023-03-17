6806a4c5f24044945b63c9d5acf1dc770bd36385
nathan-emmanuel <nathemmanuel.01@gmail.com>
2023-03-17 01:34:05 +0100

    Add: Code files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point, prints text.
+ *
+ * Return: 0(Always Successful)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle,");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point, prints text.
+ *
+ * Return: 0(Always Successful)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
