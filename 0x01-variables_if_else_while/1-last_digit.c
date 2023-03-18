b7fa299a0ce7dfc69fb08c65dd21fe937565f642
nathan-emmanuel <nathemmanuel.01@gmail.com>
2023-03-18 01:12:56 +0100

    Update: Make Betty Compliant and fixed bugs
    Some of the conditionals were checking the wrong variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,24 +3,25 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point, checks if value is greater, less or equal to some numbers
+ * main - Entry Point, checks if value is greater,
+ * less or equal to some numbers
  *
  * Return: 0 (Always)
  */
 
 int main(void)
 {
-	int n,l;
+	int n, l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
-	
+
 	if (l > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
-	else if (n == 0)
+	else if (l == 0)
 		printf("Last digit of %d is %d and is 0\n", n, l);
-	else if ((n < 6) && (n != 0))
+	else if ((l < 6) && (l != 0))
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	return (0);
 }
